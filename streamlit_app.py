09dbc0f4cd308a8e06b4727e0a1d47bff74ac2ed
ditemartin <ditemartin@gmail.com>
2024-09-16 11:50:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-
-# Inject custom CSS to control the width of the main content
-st.markdown(
-    """
-    <style>
-    .main {
-        max-width: 1000px; /* Adjust this value to set your desired width */
-        margin: 0 auto;  /* Center align the content */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 # Sample DataFrame
 data = {
     'Product': ['Product A', 'Product B', 'Product C', 'Product D'],
