297fa3bd3c4801cda39f70dd68a2438f24c1393d
ditemartin <ditemartin@gmail.com>
2024-09-16 11:35:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 # Sample DataFrame
 data = {
     'Product': ['Product A', 'Product B', 'Product C', 'Product D'],
